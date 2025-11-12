eff6b802de0b00c9dbd75445af9fefdc23b7afc8
Migueletes1 <miguel.ricaurte2020@gmail.com>
2025-11-12 15:21:48 -0500

    estructura basica

---
+++ apprentices/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApprenticesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apprentices'
---
+++ project_attachments/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProjectAttachmentsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'project_attachments'
